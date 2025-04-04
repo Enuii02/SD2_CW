98b5305e53fc633f835a01e640bffc52f9098bce
MATEUSZ_SKRZYDLO <mati200212@gmail.com>
2025-04-04 16:56:51 +0100

    Create main file

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>  // Standard input-output library
+
+// Entry point of the program
+int main() {
+    std::cout << "Hello, World!" << std::endl;  // Print message to console
+    return 0;  // Return 0 to indicate successful execution
+}
